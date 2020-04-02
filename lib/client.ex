a1b6c51f8bbcf9d9b206eee00398019974a482c2
Kamil Maciejczek <maciejczek.kamil@gmail.com>
2020-04-02 11:24:31 +0200

    use https geetest endpoint instead of http from official libs

--- lib/client.ex
+++ lib/client.ex
@@ -3,6 +3,6 @@ defmodule Geetest3.Client do
 
   adapter Tesla.Adapter.Ibrowse
 
-  plug(Tesla.Middleware.BaseUrl, "http://api.geetest.com")
+  plug(Tesla.Middleware.BaseUrl, "https://api.geetest.com")
   plug(Tesla.Middleware.JSON)
 end
