c06721ec91c13814f11d3e94c9dbf8829250d64d
Kamil Maciejczek <maciejczek.kamil@gmail.com>
2020-04-12 15:41:25 +0200

    hex package config

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Geetest3.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/terianil/geetest3"
     ]
   end
 
@@ -24,6 +25,20 @@ defmodule Geetest3.MixProject do
       {:tesla, "~> 1.3"},
       {:jason, "~> 1.0"},
       {:ibrowse, "~> 4.2"}
+    ]
+  end
+
+  defp description() do
+    "A simple server side implementation of GeeTest v3 captcha."
+  end
+
+  defp package() do
+    [
+      name: "geetest3",
+      files: ~w(lib .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/terianil/geetest3"}
     ]
   end
 end
